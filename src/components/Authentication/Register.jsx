50c9e6f4671253480f8af786b213cc4d4b9db0f8
Kin-Laptop <flinqwe@gmail.com>
2023-03-23 12:41:06 +0200

    localization

--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -99,7 +99,7 @@ const Register = () => {
                 {/*    </div>*/}
                 {/*</div>*/}
                 <button type="submit"
-                        className="w-40 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        className="w-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Створити аккаунт
                 </button>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
--- src/components/Authentication/SignIn.jsx
+++ src/components/Authentication/SignIn.jsx
@@ -73,7 +73,7 @@ const SignIn = () => {
                 </div>
                 <button type="submit"
                         className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Login to your account
+                    Авторизуватися
                 </button>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
                     Ще не зареєстровані?
